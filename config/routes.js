2e8b75c64df9061b281429f60b768927365d6966
dozoisch <hugo@dozoisch.com>
2014-06-14 22:35:10 -0400

    updated to koa-router, updated version to use 0.0.x

--- config/routes.js
+++ config/routes.js
@@ -1,13 +1,14 @@
-var router = require('koa-route');
+var router = require('koa-router');
 
 var countController = require('../src/controllers/count');
 var indexController = require('../src/controllers/index');
 
 module.exports = function (app) {
+  // register functions
+  app.use(router(app));
 
-  app.use(router.get('/', indexController.index));
-
-  app.use(router.get('/value', countController.getCount));
-  app.use(router.get('/inc', countController.increment));
-  app.use(router.get('/dec', countController.decrement));
+  app.get('/', indexController.index);
+  app.get('/value', countController.getCount);
+  app.get('/inc', countController.increment);
+  app.get('/dec', countController.decrement);
 }
