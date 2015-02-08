2bf582ff139b549f0074908f75cbc4fe6e393e75
dozoisch <hugo@dozoisch.com>
2015-02-07 23:36:45 -0500

    Fixes #10 Update to React 0.12!

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -1,42 +1,47 @@
 /** @jsx React.DOM */
-'use strict';
-var React = window.React = require('react');
-var reactNestedRouter = require('react-router');
-var Routes = reactNestedRouter.Routes;
-var Route = reactNestedRouter.Route;
-var Link = reactNestedRouter.Link;
+"use strict";
+var React = window.React = require("react");
+var Router = require("react-router");
+var RouteHandler = Router.RouteHandler;
+var Route = Router.Route;
+var DefaultRoute = Router.DefaultRoute;
+var NotFoundRoute = Router.NotFoundRoute;
+var Link = Router.Link;
 
-var IndexPage = require('./pages/index');
-var NullPage = require('./pages/null');
+var IndexPage = require("./pages/index");
+var NullPage = require("./pages/null");
 
-var container = document.getElementById('page-container');
+var container = document.getElementById("page-container");
 
 var App = React.createClass({
   render: function () {
     return (
-      <div className='row' >
-        <div className='col-md-2'>
+      <div className="row" >
+        <div className="col-md-2">
           <h3>Links</h3>
-          <ul className='nav nav-pills nav-stacked' >
-            <li><Link to='index'>Index</Link></li>
-            <li><Link to='null-page'>Null</Link></li>
+          <ul className="nav nav-pills nav-stacked" >
+            <li><Link to="index">Index</Link></li>
+            <li><Link to="null-page">Null</Link></li>
           </ul>
         </div>
-        <div className='col-md-10 well'>
-          {this.props.activeRouteHandler()}
+        <div className="col-md-10 well">
+          <RouteHandler />
         </div>
       </div>
     );
   }
 });
 
-React.renderComponent(
-  <Routes>
-    <Route handler={App} >
-      <Route name='index' path='/' handler={IndexPage} />
-      <Route name='null-page' path='/null' handler={NullPage} />
-    </Route>
-  </Routes>
-, container);
+var routes = (
+  <Route handler={App}>
+    <DefaultRoute name="index" handler={IndexPage} />
+    <Route name="null-page" path="/null" handler={NullPage} />
+    <NotFoundRoute handler={IndexPage} />
+  </Route>
+);
+
+Router.run(routes, Router.HistoryLocation, function (Handler) {
+  React.render(<Handler/>, container);
+});
 
 
